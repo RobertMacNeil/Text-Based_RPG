6b713126946318be4ed866a56b07dc2223b3058e
frozentiger279 <55792108+frozentiger279@users.noreply.github.com>
2021-02-03 10:15:46 -0400

    Created Classes, started GameCharacter

---
+++ Text_Based_RPG/Text_Based_RPG/GameCharacter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_RPG
+{
+    abstract class GameCharacter
+    {
+        // stats
+        string name;
+        int health;
+        int maxHealth;
+        int shield;
+        int maxShield;
+        int lives;
+
+        public void ShowStats()
+        {
+            Console.WriteLine(name + "'s stats:");
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Shield: " + shield);
+            Console.WriteLine("Lives: " + lives);
+        }
+
+        public void TakeDamage(int damage)
+        {
+
+        }
+
+        public void Heal(int heal)
+        {
+            //debug
+            Console.WriteLine(name + " is about to heal " + heal + " health.");
+            if(heal < 0)
+            {
+                heal = 0;
+                //debug
+                Console.WriteLine("ERROR: Must be a positve value");
+                return;
+            }
+            health = health + heal;
+        }
+
+        public void RegenerateShield(int regen)
+        {
+
+        }
+    }
+}
