7580982ad11eea1852e5c2d9ade95ba88cf42127
frozentiger279 <55792108+frozentiger279@users.noreply.github.com>
2021-02-08 00:13:35 -0400

    started wiring inheritance and finishing up GameCharacter Class

--- Text_Based_RPG/Text_Based_RPG/GameCharacter.cs
+++ Text_Based_RPG/Text_Based_RPG/GameCharacter.cs
@@ -21,12 +21,36 @@ namespace Text_Based_RPG
             Console.WriteLine(name + "'s stats:");
             Console.WriteLine("Health: " + health);
             Console.WriteLine("Shield: " + shield);
-            Console.WriteLine("Lives: " + lives);
         }
 
         public void TakeDamage(int damage)
         {
+            //debug
+            Console.WriteLine(name + " is about to take " + damage + " damage.");
+            if (damage < 0)
+            {
+                damage = 0;
+                //debug
+                Console.WriteLine("ERROR: Must be a positive value");
+                return;
+            }
+            shield = shield - damage;
+            if (shield < 0)
+            {
+                Console.WriteLine(name + " took " + damage + " damage.");
+                Console.WriteLine(name + "'s shield is broken!");
+                damage = shield * -1;
+                shield = 0;
+                health = health - damage;
+                Console.WriteLine(name + " lost " + damage + " health.");
 
+                if (health <= 0)
+                {
+                    lives = lives - 1;
+                    health = 0;
+                    ////////////////////// left off here
+                }
+            }
         }
 
         public void Heal(int heal)
@@ -37,15 +61,34 @@ namespace Text_Based_RPG
             {
                 heal = 0;
                 //debug
-                Console.WriteLine("ERROR: Must be a positve value");
+                Console.WriteLine("ERROR: Must be a positive value");
                 return;
             }
             health = health + heal;
+            if(health > maxHealth)
+            {
+                health = maxHealth;
+                Console.WriteLine(name + " has been brought to full health");
+            }
         }
 
         public void RegenerateShield(int regen)
         {
-
+            //debug
+            Console.WriteLine(name + " is about to regenerate " + regen + " shield.");
+            if (regen < 0)
+            {
+                regen = 0;
+                //debug
+                Console.WriteLine("ERROR: Must be a positive value");
+                return;
+            }
+            shield = shield + regen;
+            if (shield > maxShield)
+            {
+                shield = maxShield;
+                Console.WriteLine(name + " has regenerated full shielding");
+            }
         }
     }
 }
--- Text_Based_RPG/Text_Based_RPG/Player.cs
+++ Text_Based_RPG/Text_Based_RPG/Player.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Text_Based_RPG
 {
-    class Player
+    class Player : GameCharacter
     {
+        Player()
+        {
+
+        }
     }
 }
