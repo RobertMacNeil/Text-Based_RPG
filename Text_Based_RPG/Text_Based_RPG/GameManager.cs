3b6495802a3e1a8b894cc8c228f6180b02493c91
frozentiger279 <55792108+frozentiger279@users.noreply.github.com>
2021-02-10 01:18:13 -0400

    Started working on Map and GameManager

--- Text_Based_RPG/Text_Based_RPG/GameManager.cs
+++ Text_Based_RPG/Text_Based_RPG/GameManager.cs
@@ -10,12 +10,26 @@ namespace Text_Based_RPG
     {
         public GameManager()
         {
-            
         }
 
         public void StartGame()
         {
+            Map map = new Map();
             Player player = new Player();
+            Enemy enemy = new Enemy();
+            
+            while(true) // remember to set up with player lives
+            {
+                Console.Clear();
+
+                map.Draw();
+                //player.Draw();
+                //enemy.Draw();
+
+                //player.Update();
+                //enemy.Update();
+                Console.ReadKey();
+            }
 
         }
     }
