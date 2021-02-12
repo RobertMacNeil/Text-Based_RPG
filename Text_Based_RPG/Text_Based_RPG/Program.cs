7293a16f10421eacc75cd583e5120c31b4f1608b
frozentiger279 <55792108+frozentiger279@users.noreply.github.com>
2021-02-12 14:25:29 -0400

    Added notes to program

--- Text_Based_RPG/Text_Based_RPG/Program.cs
+++ Text_Based_RPG/Text_Based_RPG/Program.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * notes: 
+ * program inits gamemanager and runs the startgame
+ * gamemanager's startgame inits game pieces (map player enemy(ies)) loads map and holds gameloop (clear->draw->update)
+ * map has the maploader and draw method as well as holds method to detect contact (boundaries) 
+ * given to players in construct so they have access to boundaries
+ * gamecharacter holds any similar values game characters would have contains draw method of characters
+ * player and enemy inherit gamecharacter and have their own movements
+ * ***Need to find way to connect player-enemy so that they can collide***
+ * ***Have not found a way to do so while practicing OOP***
+ */
 namespace Text_Based_RPG
 {
     class Program
